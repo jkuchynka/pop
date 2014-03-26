a5cc2e4e9d2d24c4c0021785228ecf0aadb8038a
Jason Kuchynka <jason.kuchynka@gmail.com>
2014-03-25 23:55:13 -0700

    Use sqlite for testing environment when running phpunit tests

---
+++ app/config/testing/database.php
@@ -0,0 +1,15 @@
+<?php
+
+return array(
+
+  'default' => 'testing',  
+
+  'connections' => array(
+    'testing' => array(
+      'driver' => 'sqlite',
+      'database' => ':memory:',
+      'prefix' => '',
+    )
+  )
+
+);
