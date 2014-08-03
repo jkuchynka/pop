e6ec3b482203273fa7d8f58b4dd8aafce20f45a2
Jason Kuchynka <jason.kuchynka@gmail.com>
2014-08-02 17:29:42 -0700

    Fix fails in Pop Auth integration testcase

--- app/tests/Pop/integration/AuthIntegrationTest.php
+++ app/tests/Pop/integration/AuthIntegrationTest.php
@@ -4,63 +4,63 @@ use Woodling\Woodling;
 
 class AuthIntegrationTest extends TestCase {
 
-  public function testLoginSuccessReturnsUserObject()
-  {
-    $user = Woodling::saved('User');
-    $response = $this->call('POST', '/api/auth', [
-      'email' => $user->email,
-      'password' => 'password'
-    ]);
-    $data = $this->assertResponse($response);
-    $this->assertUser($user, $data);
-  }
+    public function testLoginSuccessReturnsUserObject()
+    {
+        $user = Woodling::saved('User');
+        $response = $this->call('POST', '/api/auth', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+        $data = $this->assertResponse($response);
+        $this->assertModel($user, $data);
+    }
 
-  // Should be able to login with username or password
-  public function testLoginWithUsername()
-  {
-    $user = Woodling::saved('User');
-    $response = $this->call('POST', '/api/auth', array(
-      'email' => $user->username,
-      'password' => 'password'
-    )); 
-    $data = $this->assertResponse($response);
-    $this->assertUser($user, $data);
-  }
+    // Should be able to login with username or password
+    public function testLoginWithUsername()
+    {
+        $user = Woodling::saved('User');
+        $response = $this->call('POST', '/api/auth', [
+            'email' => $user->username,
+            'password' => 'password'
+        ]);
+        $data = $this->assertResponse($response);
+        $this->assertModel($user, $data);
+    }
 
-  public function testLoginFailureReturnsFailResponse()
-  {
-    $response = $this->call('POST', '/api/auth', array(
-      'email' => 'foo@bar.net',
-      'password' => 'failme'
-    ));
-    $this->assertResponse($response, true);
-  }
+    public function testLoginFailureReturnsFailResponse()
+    {
+        $response = $this->call('POST', '/api/auth', [
+            'email' => 'foo@bar.net',
+            'password' => 'failme'
+        ]);
+        $this->assertResponse($response, true);
+    }
 
-  public function testGetCurrentLoggedInUserReturnsUserObject()
-  {
-    $user = Woodling::saved('User');
-    $response = $this->call('POST', '/api/auth', array(
-      'email' => $user->email,
-      'password' => 'password'
-    ));
-    $response = $this->call('GET', '/api/auth/current');
-    $data = $this->assertResponse($response);
-    $this->assertUser($user, $data);
-  }
+    public function testGetCurrentLoggedInUserReturnsUserObject()
+    {
+        $user = Woodling::saved('User');
+        $response = $this->call('POST', '/api/auth', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+        $response = $this->call('GET', '/api/auth/current');
+        $data = $this->assertResponse($response);
+        $this->assertModel($user, $data);
+    }
 
-  public function testLogoutCurrentUserReturnsGuestUserObject()
-  {
-    $user = Woodling::saved('User');
-    $response = $this->call('POST', '/api/auth', array(
-      'email' => $user->email,
-      'password' => 'password'
-    ));
-    $response = $this->call('GET', '/api/auth/logout');
-    $data = $this->assertResponse($response);
-    $response = $this->call('GET', '/api/auth/current');
-    $data = $this->assertResponse($response);
-    $this->assertEmpty($data->id);
-    $this->assertEquals('guest', $data->username);
-  }
+    public function testLogoutCurrentUserReturnsGuestUserObject()
+    {
+        $user = Woodling::saved('User');
+        $response = $this->call('POST', '/api/auth', [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+        $response = $this->call('DELETE', '/api/auth/me');
+        $data = $this->assertResponse($response);
+        $response = $this->call('GET', '/api/auth/me');
+        $data = $this->assertResponse($response);
+        $this->assertEmpty($data->id);
+        $this->assertEquals('guest', $data->username);
+    }
 
 }
